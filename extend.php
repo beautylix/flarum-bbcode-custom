2d28569b7b22c29d7177f6bfe4f3aa4b8f4fa2c3
beautylix <tibywang@gmail.com>
2022-08-11 13:04:45 +0800

    initial
    initial

--- extend.php
+++ extend.php
@@ -12,15 +12,24 @@ use Flarum\Frontend\Document;
 use s9e\TextFormatter\Configurator;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->content(function (Document $document) {
-            $document->head[] = '<link rel="stylesheet" type="text/css" href="/assets/extensions/YOURUSERNAME-nameofextension/styles.css">';
-        }),
+
+/*
+ *   (new Extend\Frontend('forum'))
+ *       ->content(function (Document $document) {
+ *           $document->head[] = '<link rel="stylesheet" type="text/css" href="/assets/extensions/YOURUSERNAME-nameofextension/styles.css">';
+ *       }),
+ */
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
-                '[bbcode="{TEXT1}"]{TEXT2}[/bbcode]',
-                '<span class="{TEXT1}">{TEXT2}</span>'
+                '[sup]{TEXT}[/sup]',
+                '<sup>{TEXT}</sup>'
             );
+
+            $config->BBCodes->addCustom(
+                '[sub]{TEXT}[/sub]',
+                '<sub>{TEXT}</sub>'
+            );			
+			
         })
 ];
